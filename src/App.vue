d18695bdd1d5a9ab00a63ece9cb218201cb66622
xiyou <xiyou787@waq.cn>
2023-03-03 16:48:35 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <a :href="url">点击去{{ name }}</a>
+    <input type="text" v-model="name" />
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "百度",
+      url: "www.baidu.com",
+    };
+  },
+};
 </script>
 
 <style>
